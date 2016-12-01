63d3b1e24c07d566e96b6916e95273a64db1a7f1
Alex <alex@alexs-macbook-pro.local>
2016-12-01 18:20:28 +0200

    new branch

--- Enum and Table/Enum and Table/ViewController.swift
+++ Enum and Table/Enum and Table/ViewController.swift
@@ -14,6 +14,8 @@ enum Numbers: Int, Countable {
     static let names = [one: "One", two: "Two", three: "Three", fouth: "Fouth"]
 }
 
+
+
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
